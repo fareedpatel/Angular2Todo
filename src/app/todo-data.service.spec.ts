cc09a6ea9c0578861bb682c06909650a57b9b1e9
Fareed Patel <fareed_patel@hotmail.co.uk>
2017-07-17 22:31:04 +0100

    save todo spec passing

--- src/app/todo-data.service.spec.ts
+++ src/app/todo-data.service.spec.ts
@@ -13,18 +13,33 @@ describe('TodoDataService', () => {
     expect(service).toBeTruthy();
   }));
 
-describe('#getAllTodos()', () => {
-  it('should return an empty array by default', inject([TodoDataService], (service:TodoDataService) => {
-    expect(service.getAllTodos()).toEqual([]);
-  }));
-  it('should return all todos', inject([TodoDataService], (service: TodoDataService) => {
-    let todo1 = new Todo({title: 'Hello1', complete: false});
-    let todo2 = new Todo({title: 'Hello2', complete: true});
+  describe('#getAllTodos()', () => {
+    it('should return an empty array by default', inject([TodoDataService], (service:TodoDataService) => {
+      expect(service.getAllTodos()).toEqual([]);
+    }));
+    it('should return all todos', inject([TodoDataService], (service: TodoDataService) => {
+      let todo1 = new Todo({title: 'Hello1', complete: false});
+      let todo2 = new Todo({title: 'Hello2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    }));
+  });
+
+  describe('#save(todo)', () => {
+
+     it('should automatically assign an incrementing id', inject([TodoDataService], 
+  (service: TodoDataService ) => {
+    let todo1 = new Todo({title: 'Hello 1', complete: false});
+    let todo2 = new Todo({title: 'Hello 2', complete: true});
     service.addTodo(todo1);
     service.addTodo(todo2);
-    expect(service.getAllTodos()).toEqual([todo1, todo2]);
-  }));
-});
+    expect(service.getTodoById(1)).toEqual(todo1);
+    expect(service.getTodoById(2)).toEqual(todo2);
+    }));
+   });
+
+
 });
 
 
